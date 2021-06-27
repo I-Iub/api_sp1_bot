ea6eb96e87c8fef7737dd8544e5c7fb0f3c68af1
Рашид <ri-post@yandex.ru>
2021-06-27 19:05:00 +0500

    Версия 1

--- homework.py
+++ homework.py
@@ -4,7 +4,6 @@ import time
 import requests
 import telegram
 from dotenv import load_dotenv
-import json
 import tg_logger
 
 load_dotenv()
@@ -15,7 +14,7 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename='program.log', 
+    filename='program.log',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 )
 logger = logging.getLogger(__name__)
@@ -25,19 +24,17 @@ stream_handler = logging.StreamHandler()
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
-logger_for_tg = logging.getLogger(__name__)
-logger_for_tg.setLevel(logging.ERROR)
-tg_logger.setup(logger_for_tg, token=TELEGRAM_TOKEN, users=[CHAT_ID])
+tg_logger.setup(logger, token=TELEGRAM_TOKEN, users=[CHAT_ID])
 
 # проинициализируйте бота здесь,
 # чтобы он был доступен в каждом нижеобъявленном методе,
 # и не нужно было прокидывать его в каждый вызов
-bot = telegram.Bot(token=TELEGRAM_TOKEN)  # !
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']  # !
-    if homework['status'] == 'rejected':  # !
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -45,10 +42,10 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'  # !
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}  # !
-    payload = {'from_date': current_timestamp}  # !
-    homework_statuses = requests.get(url, headers=headers, params=payload)  # !
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    homework_statuses = requests.get(url, headers=headers, params=payload)
     return homework_statuses.json()
 
 
@@ -63,17 +60,21 @@ def main():
     logger.debug('Bot launched')
     while True:
         try:
-            homeworks = get_homeworks(current_timestamp - 2629743)  # !
-            homework = homeworks['homeworks'][0]  # !
+            # получаем домашки за последний месяц
+            homeworks = get_homeworks(current_timestamp - 2629743)
+            homework = homeworks['homeworks'][0]  # извлекаем последнюю домашку
+            # проверяем нет ли изменений в состоянии домашки,
+            # исключаем, сравнение состояний при первом запуске,
+            # когда current_status, очевидно, отличается от очередной
+            # запрошенной домашки:
             if (current_status['homework'] != homework
-               and current_status['homework'] != None):
+               and current_status['homework'] is not None):
                 logger.info('Изменение статуса домашки')
-                message = parse_homework_status(homework)  # !
-                send_message(message)  # !
+                message = parse_homework_status(homework)
+                send_message(message)
+                # созраняем последнее состояние домашки, чтобы
+                # сравнивать с полученной в очередном запросе домашкой:
                 current_status['homework'] = homework
-
-            # homework = json.dumps(homeworks)
-            # send_message(homework)
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
         except Exception as e:
